01d4e3b1d8b6b6567d95e49829ed56222e5c284a
Andrey Lozovoy <lozovoya@gmail.com>
2020-07-12 19:56:56 +0300

    random amount generator added

--- cmd/main.go
+++ cmd/main.go
@@ -4,14 +4,21 @@ import (
 	"fmt"
 	"github.com/lozovoya/gohomework6_1/pkg/card"
 	"github.com/lozovoya/gohomework6_1/pkg/operations"
+	"math/rand"
 )
 
 func main() {
-	amounts := []int64{1_000_00, 3_000_00, 7_000_00, 2_000_00, 11_000_00, 1_000_00}
+	//amounts := []int64{1_000_00, 3_000_00, 7_000_00, 2_000_00, 11_000_00, 1_000_00}
+
+	amounts := make([]int64, 100)
 	penBank := *card.NewService("PengiunBank")
 
 	penBank.AddCard("visa", 1_000_00, "rub", "http://...", "5106 2111 1111 1116")
 	penBank.AddCard("visa", 1_000_000_00, "rub", "http://...", "5106 2100 0000 0003")
+
+	for index := range amounts {
+		amounts[index] = int64(rand.Intn(1_000_000))
+	}
 
 	for _, amount := range amounts {
 		err := penBank.AddTransaction("5106 2111 1111 1116", amount, "1234")
